5c7a7e6232b864270f94055af8abea54275712e6
ATIKSH09 <atikshgupta6373@gmail.com>
2025-06-02 16:48:28 +0530

    feat add docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: true, // this makes Vite listen on 0.0.0.0
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
